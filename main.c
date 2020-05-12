7d8731c87351b550af693d6153b842681194aaba
nickarshadi <1477@holbertonschool.com>
2020-05-12 23:03:32 +0000

    Reading file line by line

--- main.c
+++ main.c
@@ -2,23 +2,29 @@
 
 int main(int argc, char **argv)
 {
-	int fd;
-	char buf[1024 * 0xa];
-	ssize_t bytes;
+	FILE *fp;
+	//char buf[1024 * 0xa];
+	ssize_t read;//, bytes = 0;
+	char *line = NULL;
+	size_t len = 0;
 
 	if (!argv[1] || argc != 2)
 	{
 		perror("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == (-1))
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
 	{
 		perror("Error: Can't open file <file>\n");
 		exit(EXIT_FAILURE);
 	}
-	bytes = read(fd, &buf[0], 1024);
-	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-	close(fd);
+	while ((read = getline(&line, &len, fp)) != -1)
+	{
+		printf("%s\n", line);
+	}
+		free(line);
+
+	//bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	return (1);
 }
