6fd15b4880907807a020de3d58283232dc598937
nickarshadi <1477@holbertonschool.com>
2020-05-13 16:03:18 +0000

    Betty

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include "monty.h"
 
+/**
+  * main - run monty bytecode
+  * @argc: number of arguments
+  * @argv: Array of argument strings
+  * Return: 1
+  */
 int main(int argc, char **argv)
 {
 	FILE *fp;
 	ssize_t read;
 	char *line = NULL;
 	size_t len = 0;
+	int i;
 
 	if (!argv[1] || argc != 2)
 	{
@@ -15,11 +22,12 @@ int main(int argc, char **argv)
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n",argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while ((read = getline(&line, &len, fp)) != -1)
 	{
+		i++;
 		printf("%s", line);
 	}
 	free(line);
